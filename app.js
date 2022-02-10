2703fd9c84d9a7cae49daceec1b2680c6c7d322c
nelly236 <nynelly5@gmail.com>
2022-02-10 10:09:22 +0100

    feat: fonction calcul

--- app.js
+++ app.js
@@ -44,3 +44,39 @@ function multiplication(nb1, nb2){
    let calcul = nb1*nb2;
    alert(calcul);
 }
+//calculatrice
+
+
+function calculatrice(){
+    let operation = prompt("Bienvenue dans la super calculatrice\n\nVeuillez choisir une option :\n1) Tapez 1 pour réaliser une addition\n2) Tapez 2 pour réaliser une soustraction\n3) Tapez 3 pour réaliser une division\n4) Tapez 4 pour réaliser une multiplication");
+     
+       if(operation == "1"){
+       demandeNombre();
+       verifieNombre(nb1, nb2);
+       convertirNombre(nb1,nb2);
+       addition (nbConvert1, nbConvert2);
+       }
+       else if (operation == "2" ){
+       demandeNombre();
+       verifieNombre(nb1, nb2);
+       convertirNombre(nb1,nb2);
+       soustraction(nbConvert1, nbConvert2);
+       }
+       else if (operation == "3"){
+       demandeNombre();
+       verifieNombre(nb1, nb2);
+       convertirNombre(nb1,nb2);
+       division(nbConvert1, nbConvert2);
+       }
+       else if (operation == "4"){
+       demandeNombre();
+       verifieNombre(nb1, nb2);
+       convertirNombre(nb1,nb2);
+       multiplication(nbConvert1, nbConvert2);
+       }
+       else{
+          if(alert("Veuillez saisir une opération valide. Appuyer sur 'OK' pour recommencer ou 'Annuler' pour quitter")){
+             calculatrice();
+          }
+       } 
+    }
